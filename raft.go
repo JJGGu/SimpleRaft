1ef40a31fd196b90ae1633b4d3ff13cc0ae36656
xuhaoran <xuhaoran.1996@bytedance.com>
2021-01-31 22:08:09 +0800

    实现不同节点运行逻辑

--- raft.go
+++ raft.go
@@ -1,6 +1,7 @@
 package raft
 
 import (
+	"errors"
 	"log"
 	"net"
 	"os"
@@ -51,7 +52,7 @@ type RaftNode struct {
 	commitTerm int
 	lastApplied int
 
-	// 发送RPC
+	// 用于随机选举超时接收信号
 	toCh chan int
 	// 用于传输RPC
 	rpcCh chan *RaftRPC
@@ -100,21 +101,121 @@ func initRaft(config *RaftConfig) *RaftNode {
 }
 
 // 该节点以FOLLOWER状态运行，直到状态发生变化
-func (r *RaftNode) runFollower() {
-
+func (r *RaftNode) runFollower() error {
+	go randomTimeout(r.toCh)
+	r.logger.Printf("[INFO] %s running as follower", r.config.addr)
+	for {
+		select {
+		case to := <- r.toCh:
+			if to == TimeRunOut{
+				log.Printf("时间用完了,转变为candidate")
+				r.state = CANDIDATE
+				r.currentTerm += 1
+			}
+		case rpc := <- r.rpcCh:
+			r.handleRPC(rpc)
+		}
+	}
 }
 
 // 该节点以CANDIDATE状态运行，直到状态发生变化
 func (r *RaftNode) runCandidate() {
+	r.voteCount = 1
+	electionTimeout := 0
+	r.votedFor = r.config.addr
+	majority := len(r.config.members) / 2
 
+	for _, member := range r.config.members {
+		go r.SendRequestVote(member)
+	}
+	// read from rpcCh until majority is achieved
+	// if gets AppendEntries or VoteReq with > term revert to follower
+	// else keep counting votes
+	for r.state == CANDIDATE {
+		go randomTimeout(r.toCh)
+		select {
+		case rpc := <-r.rpcCh:
+			r.logger.Printf("[INFO] candidate received rpc")
+			r.handleRPC(rpc)
+			if r.state != CANDIDATE {
+				return
+			}
+		case _ = <-r.toCh:
+			r.logger.Printf("[INFO] %s received %d votes", r.config.addr, r.voteCount)
+			electionTimeout += 1
+			go randomTimeout(r.toCh)
+		}
+		// 转变为Leader
+		if r.voteCount > majority {
+			r.logger.Printf("[LEADER] taking power %s", r.config.addr)
+			r.state = LEADER
+			return
+		}
+		// 当前term未选出，重新开始选
+		if electionTimeout >= 2 {
+			r.currentTerm += 1
+			electionTimeout = 0
+			r.voteCount = 1
+			r.logger.Printf("[INFO] term is %d", r.currentTerm)
+			for _, member := range r.config.members {
+				go r.SendRequestVote(member)
+			}
+		}
+	}
 }
 
 // 该节点以LEADER状态运行，直到状态发生变化
 func (r *RaftNode) runLeader() {
-
+	r.logger.Printf("[INFO] %s is now leader", r.config.addr)
+	go randomTimeout(r.toCh)
+	for r.state == LEADER {
+		// 发送心跳(存放日志信息）
+		for _, member := range r.config.members {
+			go r.SendHeartbeat(member)
+		}
+		select {
+		case rpc := <-r.rpcCh:
+			r.handleRPC(rpc)
+		case _ = <-r.toCh:
+			for _, member := range r.config.members {
+				go r.SendHeartbeat(member)
+			}
+		}
+	}
 }
 
-// 用于处理RPC请求与响应
+// 用于处理不同类型的RPC请求与响应
 func (r *RaftNode) handleRPC(rpc *RaftRPC) {
+	switch rpc.St.(type) {
+	case AppendEntries:
+		r.HandleAppendEntries(rpc.St.(AppendEntries))
+	case AppendEntriesResponse:
+		r.HandleAppendEntriesResponse(rpc.St.(AppendEntriesResponse))
+	case RequestVote:
+		r.HandleRequestVote(rpc.St.(RequestVote))
+	case RequestVoteResponse:
+		r.HandleRequestVoteResponse(rpc.St.(RequestVoteResponse), &r.voteCount)
+	case ClientRequest:
+		r.HandleClientRequest(rpc.St.(ClientRequest))
+	default:
+		r.logger.Printf("[ERROR] 未知 RPC: %+v\n", rpc)
+	}
+}
 
+func (r *RaftNode) RunRaft() error {
+	// 监听端口
+	go r.RunTCPServer()
+
+	for {
+		if r.state == FOLLOWER {
+			r.runFollower()
+		} else if r.state == CANDIDATE {
+			r.runCandidate()
+		} else if r.state == LEADER {
+			r.runLeader()
+		} else {
+			return errors.New("state error")
+		}
+
+	}
 }
--- utils.go
+++ utils.go
@@ -1,8 +1,29 @@
 package raft
 
-import "strings"
+import (
+	"math/rand"
+	"strings"
+	"time"
+)
+
+// 随机选举超时时间50-300ms
+const TimeoutMax = 300
+const TimeoutMin = 50
+const TimeRunOut = 0
 
 func getPort(addr string) string {
 	parts := strings.Split(addr, ":")
 	return parts[len(parts) - 1]
 }
+
+func random(min, max int) int {
+	return rand.Intn(max - min) + min
+}
+
+// 时间一到就输入到tochan
+func randomTimeout(toChan chan int) {
+	randomTime := time.Duration(random(TimeoutMin, TimeoutMax))
+	time.Sleep(randomTime * time.Millisecond)
+	toChan <- TimeRunOut
+}
+
